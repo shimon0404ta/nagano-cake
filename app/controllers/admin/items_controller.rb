672697bba4fe6719d49ee521bd0d93e1b5f80d2a
TakumiShimotsuka <124693530+shimon0404ta@users.noreply.github.com>
2023-05-07 18:03:05 +0000

    [Fix] 管理者用の商品関連のページを修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,18 +3,8 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
   end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to admin_item_path(@item.id)
-    else
-      flash[:item_create_error] = "項目は必ずご入力ください"
-      redirect_to new_admin_item_path
-    end
-  end
-
   def index
-    @items = Item.page(params[:page])
+    @items = Item.all
   end
 
   def show
@@ -25,19 +15,28 @@ class Admin::ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path(item.id)
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to admin_items_path
+  end
+
   def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to admin_item_path(@item.id)
-    else
-      flash[:item_update_error] = "項目は必ずご入力ください"
-      redirect_to edit_admin_item_path(@item.id)
-    end
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)
   end
 
   private
 
   def item_params
-    #params.require(:item).permit(:name, :introduction, :price, :genre_id)
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :status, :image)
   end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,19 @@ class Item < ApplicationRecord
   has_many :cart_items
   has_many :order_details
   belongs_to :genre
+  
+  enum status: { buy_now: true, sold_out: false}
+  
+  def get_item_image(width,height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      image.attach(io: File.open(file_path), filename: 'default_image.jpeg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height])
+  end
+
+  def with_tax_price
+    (price*1.1).floor
+  end
 
 end
